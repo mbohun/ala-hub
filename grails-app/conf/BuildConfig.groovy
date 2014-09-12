0832f2335087ddcc93eff00db6540cba35469c7f
mbohun <martin.bohun@gmail.com>
2014-09-12 16:11:18 +1000

    added snapshot and release maven repos; adding this to BuildConfig.groovy has the advantage that we do not have to create on travis-ci.org a separate ~/.grails/settings.groovy file

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -68,6 +68,10 @@ grails.project.dependency.resolution = {
     }
 }
 
-grails.project.repos.testRepo.url      = "scp://maven.ala.org.au:/data/maven"
-grails.project.repos.testRepo.username = System.getenv("TRAVIS_DEPLOY_USERNAME")
-grails.project.repos.testRepo.password = System.getenv("TRAVIS_DEPLOY_PASSWORD")
+grails.project.repos.'nectar-nexus-repo-snapshot'.url = "http://130.56.249.242/nexus/content/repositories/snapshots/"
+grails.project.repos.'nectar-nexus-repo-snapshot'.username = System.getenv("TRAVIS_DEPLOY_USERNAME")
+grails.project.repos.'nectar-nexus-repo-snapshot'.password = System.getenv("TRAVIS_DEPLOY_PASSWORD")
+
+grails.project.repos.'nectar-nexus-repo-release'.url = "http://130.56.249.242/nexus/content/repositories/releases/"
+grails.project.repos.'nectar-nexus-repo-release'.username = System.getenv("TRAVIS_DEPLOY_USERNAME")
+grails.project.repos.'nectar-nexus-repo-release'.password = System.getenv("TRAVIS_DEPLOY_PASSWORD")
